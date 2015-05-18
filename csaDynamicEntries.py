363f3dd2bc182ea1fcbd728b08b9d4bfcedf0c17
René Ribaud <rene.ribaud@hp.com>
2015-05-18 18:52:27 +0200

    Commit new stuffs.

---
+++ csaDynamicEntries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+print """
+<Property>
+
+<availableValues>
+<displayName>Marketing</displayName>
+<description></description>
+<value>VLANFE=2251;VLANBE=2261</value>
+</availableValues>
+
+<availableValues>
+<displayName>IT</displayName>
+<description></description>
+<value>VLANFE=2252;VLANBE=2262</value>
+</availableValues>
+
+<availableValues>
+<displayName>HR</displayName>
+<description></description>
+<value>VLANFE=2253;VLANBE=2263</value>
+</availableValues>
+
+
+</Property>
+"""
