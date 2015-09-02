6c6016a0d531143b8afbc10b1ff06477924dea6e
René Ribaud <rene.ribaud@hp.com>
2015-09-02 13:23:03 +0200

    Add "deployed" date.

--- failures_generator/index.php
+++ failures_generator/index.php
@@ -10,6 +10,7 @@
 
   <link rel="stylesheet" href="css/general.css">
   <link rel="stylesheet" href="css/hp.css">
+  <meta http-equiv="refresh" content="10"> 
 
 </head>
 
@@ -21,7 +22,6 @@ Click the following buttons to generate a failure.
 <br/>
 <br/>
 <?php
-#$file = "/home/sdi/ansible/essai/datafile.json";
 $file = "datafile.json";
 
 $json = file_get_contents($file);
@@ -42,32 +42,34 @@ foreach($data as $service){
 		if( preg_match("/web\d-client$/", $key, $matches)){
 			$hostkey = $key . "-hostname";
 			if(isset($service[$hostkey])){
-				$web_servers[$value] = $service[$hostkey];
+				$web_servers[$value]["hostname"] = $service[$hostkey];
+				$web_servers[$value]["lastupdate"] = $service["lastupdate"];
 				}
 			else{
 				$web_servers[$value] = "";
+				$web_servers[$value]["lastupdate"] = $service["lastupdate"];
 			}
 		}
 	}
-
 }
 
 #print var_dump($web_servers);
 # Now print the data
-foreach($web_servers as $ip => $hostname){
-	printf("<h2>Server : %s (%s)</h2>\n", $ip, $hostname);
+foreach($web_servers as $ip => $values){
+	printf("<h2>Server : %s (%s)</h2>\n", $ip, $values["hostname"]);
+        printf("Deployed : %s\n", $values["lastupdate"]);
 	printf("<img src=\"images/moonshot.png\" style=\"width:150px\">\n");
 	printf("<br/>\n");
 	printf("<br/>\n");
 	printf("<form action=\"stress.php\" method=\"POST\">\n");
 	printf("<input type=\"hidden\" name=\"ip\" value=\"%s\">\n",$ip);
-	printf("<input type=\"hidden\" name=\"system\" value=\"%s\">\n",$hostname);
+	printf("<input type=\"hidden\" name=\"system\" value=\"%s\">\n",$values["hostname"]);
 	printf("<input type=\"submit\" value=\"Stress\">\n");
 	printf("</form>\n");
 	printf("<br/>\n");
 	printf("<form action=\"stop.php\" method=\"POST\">\n");
 	printf("<input type=\"hidden\" name=\"ip\" value=\"%s\">\n",$ip);
-	printf("<input type=\"hidden\" name=\"system\" value=\"%s\">\n",$hostname);
+	printf("<input type=\"hidden\" name=\"system\" value=\"%s\">\n",$values["hostname"]);
 	printf("<input type=\"submit\" value=\"Stop web service\">\n");
 	printf("</form>\n");
 	printf("<br/>\n");
